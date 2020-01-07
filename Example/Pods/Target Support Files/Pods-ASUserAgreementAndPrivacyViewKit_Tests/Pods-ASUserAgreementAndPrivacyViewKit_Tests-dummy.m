40d62a77c48b35f06f72afe9d2a0873fa57840ac
李正林 <614544959@qq.com>
2020-01-07 16:14:57 +0800

    发布用户协议和隐私政策的弹窗1.0.0版本

---
+++ Example/Pods/Target Support Files/Pods-ASUserAgreementAndPrivacyViewKit_Tests/Pods-ASUserAgreementAndPrivacyViewKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASUserAgreementAndPrivacyViewKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ASUserAgreementAndPrivacyViewKit_Tests
+@end
